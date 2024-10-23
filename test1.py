ebddb42892a722c52c74a9d810683a632e9175ff
mfathul21 <muhammadfathul386@gmail.com>
2024-10-23 21:33:06 +0700

    Version 1

---
+++ test1.py
@@ -0,0 +1,14 @@
+import pdb
+
+
+produk_motor = {
+    "Suzuki Hayabusa": "2005",
+    "Ducati Monster": "2012",
+    "Honda CB1000": "2013",
+}
+
+for produk, tahun in produk_motor.items():
+    print(f"Motor Sport: {produk} diproduksi pada tahun {produk}")
+
+
+print("Hello")
